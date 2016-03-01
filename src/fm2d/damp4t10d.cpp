c48f809ae6c3dccc7f1e91b288928e419a1b48f7
Conghui He <heconghui@gmail.com>
2016-03-01 20:21:27 +0800

    do not transform velocity

--- src/fm2d/damp4t10d.cpp
+++ src/fm2d/damp4t10d.cpp
@@ -13,82 +13,6 @@ extern "C" {
 #include <rsf.h>
 }
 
-//static void fd4t10s_bd1_2d(float *prev_wave, const float *curr_wave) {
-//  float a[6];
-//
-//  int d = 5;
-//
-//  int ix, iz, curPos;
-//
-//  /// Zhang, Jinhai's method
-//  a[0] = +1.53400796;
-//  a[1] = +1.78858721;
-//  a[2] = -0.31660756;
-//  a[3] = +0.07612173;
-//  a[4] = -0.01626042;
-//  a[5] = +0.00216736;
-//
-//  float max_delta = 0.05;
-//
-//  int nx =
-//  std::vector<float> u2(dim.nx * dim.nz, 0);
-//
-//  #pragma omp parallel for schedule(dynamic) private(ix, iz, curPos)
-//  for (ix = d; ix < dim.nx - d; ix ++) {
-//    for (iz = d; iz < dim.nz - d; iz ++) {
-//      curPos = idx_in_cube(dim, ix, 0, iz);
-//      u2[curPos] = -4.0 * a[0] * curr_wave[curPos] +
-//                   a[1] * (curr_wave[curPos - 1]  +  curr_wave[curPos + 1]  +
-//                           curr_wave[curPos - dim.nz]  +  curr_wave[curPos + dim.nz])  +
-//                   a[2] * (curr_wave[curPos - 2]  +  curr_wave[curPos + 2]  +
-//                           curr_wave[curPos - 2 * dim.nz]  +  curr_wave[curPos + 2 * dim.nz])  +
-//                   a[3] * (curr_wave[curPos - 3]  +  curr_wave[curPos + 3]  +
-//                           curr_wave[curPos - 3 * dim.nz]  +  curr_wave[curPos + 3 * dim.nz])  +
-//                   a[4] * (curr_wave[curPos - 4]  +  curr_wave[curPos + 4]  +
-//                           curr_wave[curPos - 4 * dim.nz]  +  curr_wave[curPos + 4 * dim.nz])  +
-//                   a[5] * (curr_wave[curPos - 5]  +  curr_wave[curPos + 5]  +
-//                           curr_wave[curPos - 5 * dim.nz]  +  curr_wave[curPos + 5 * dim.nz]);
-//    }
-//  }
-//
-//  #pragma omp parallel for schedule(dynamic) private(ix, iz, curPos)
-//  for (ix = d + 1; ix < dim.nx - d - 1; ix ++) { /// the range of ix is different from that in previous for loop
-//    for (iz = d + 1; iz < dim.nz - d - 1; iz ++) { /// be careful of the range of iz
-//      float delta;
-//      float dist = 0;
-//      if (ix >= dim.bx && ix < dim.nx - dim.bx &&
-//          iz < dim.nz - dim.bz) {
-//        dist = 0;
-//      }
-//      if (ix < dim.bx) {
-//        dist = (float)(dim.bx - ix) / dim.bx;
-//      }
-//      if (ix >= dim.nx - dim.bx) {
-//        dist = (float)(ix - (dim.nx - dim.bx)  +  1) / dim.bx;
-//      }
-//      if (iz >= dim.nz - dim.bz) {
-//        dist = (float)(iz - (dim.nz - dim.bz) + 1) / dim.bz;
-//      }
-//
-//      delta = max_delta * dist * dist;
-//
-//      curPos = idx_in_cube(dim, ix, 0, iz);
-//      prev_wave[curPos] = (2. - 2 * delta + delta * delta) * curr_wave[curPos] - (1 - 2 * delta) * prev_wave[curPos]  +
-//                          (1.0f / vel[curPos]) * u2[curPos] + /// 2nd order
-//                          1.0f / 12 * (1.0f / vel[curPos]) * (1.0f / vel[curPos]) *
-//                          (u2[curPos - 1] + u2[curPos + 1] + u2[curPos - dim.nz] + u2[curPos + dim.nz] - 4 * u2[curPos]); /// 4th order
-//    }
-//  }
-//}
-
-
-static void numericTrans(Velocity &vel, float dx, float dt) {
-  std::vector<float> &vv = vel.dat;
-  for (size_t i = 0; i < vv.size(); i ++) {
-    vv[i] = (dx * dx) / (vv[i] * vv[i] * dt * dt);
-  }
-}
-
 static void expandForStencil(Velocity &exvel, const Velocity &v0, int halo) {
   int nx = v0.nx;
   int nz = v0.nz;
@@ -166,12 +90,6 @@ Damp4t10d::Damp4t10d(float _dt, float _dx, int _nb) :
 
 Velocity Damp4t10d::transformVelocityForModeling(const Velocity& _vel) {
   Velocity vv = _vel;
-
-  DEBUG() << format("nx %d, nz %d") % vv.nx % vv.nz;
-  DEBUG() << format("before transform sum vv %.20f") % sum(vv.dat);
-  numericTrans(vv, dx, dt);
-
-  DEBUG() << format("sum vv %.20f") % sum(vv.dat);
 
   // expand for boundary
   Velocity exvelForBndry(vv.nx + 2 * nb, vv.nz + nb);
@@ -275,7 +193,8 @@ void Damp4t10d::fd4t10s_bd1_2d(float* prev_wave, const float* curr_wave) {
       delta = max_delta * dist * dist;
 
       curPos = ix * nz + iz;
-      float curvel = vel->dat[curPos];
+      float vv = vel->dat[curPos];
+      float curvel = (dx * dx) / (vv * vv * dt * dt);
       prev_wave[curPos] = (2. - 2 * delta + delta * delta) * curr_wave[curPos] - (1 - 2 * delta) * prev_wave[curPos]  +
                           (1.0f / curvel) * u2[curPos] + /// 2nd order
                           1.0f / 12 * (1.0f / curvel) * (1.0f / curvel) *
