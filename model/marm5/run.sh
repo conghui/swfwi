fec0479b817b0d5d4bbb3eed26c64e17da1097e8
Conghui He <heconghui@gmail.com>
2016-04-11 14:53:51 +0800

    update run.sh

--- model/marm5/run.sh
+++ model/marm5/run.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
+# vim: fdm=marker fdl=0
+#
+# usage example: ./run.sh fm 8
+#
 
 # it is critical important to set this environment variable
 # in order to keep everything in the corrent folder
 export DATAPATH=`pwd`/
 
-# only works in GNU or INTEL compiler
-export OMP_NUM_THREADS=8
+allowed_tasks=( \
+  "fm" "essfwi" "enfwi" "noise" \
+  "fm-sw" "essfwi-sw" "enfwi-sw" "noise-sw" \
+  )
 
-# possible option:
-# fm,     essfwi,     enfwi,    noise
-# fm-sw,  essfwi-sw,  enfwi-sw, noise-sw
-#
-scons task=fm
+# check the parameters#{{{
+task=$1
+task_is_valid=0
+for t in ${allowed_tasks[@]}; do
+  if [[ $task == $t ]]; then task_is_valid=1; fi
+done
+if [[ $task_is_valid -eq 0 ]]; then
+  echo "usage: $0 task [nthreads]"
+  echo "allowed_tasks: ${allowed_tasks[@]}"
+  echo "nthreads only used in GNU or Intel compilers"
+  exit
+fi
+#}}}
+# set omp threads#{{{
+if [[ -z $2 ]]; then  nthread=8; else  nthread=$2; fi
+export OMP_NUM_THREADS=$nthread
+#}}}
+
+scons task=$1
