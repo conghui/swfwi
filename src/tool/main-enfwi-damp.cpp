268611572489a7f26fd230c6bf7b6d3439a99d65
Bingwei <chenbwei2012@gmail.com>
2016-05-31 11:32:20 +0800

    parallel io but remains some bugs

--- src/tool/main-enfwi-damp.cpp
+++ src/tool/main-enfwi-damp.cpp
@@ -206,6 +206,45 @@ std::vector<Velocity *> createVelDB(const Velocity &vel, const char *perin, int 
   return veldb;
 }
 
+std::vector<Velocity *> pCreateVelDB(const Velocity &vel, const char *perin, int N, float dx, float dt, int rank, int size) {
+  std::vector<Velocity *> veldb(N);   /// here is all the velocity samples resides, others are pointer to this
+
+  int modelSize = vel.nx * vel.nz;
+	MPI_File fh;
+	MPI_Offset file_size, offset;
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_File_open(MPI_COMM_WORLD, perin, MPI_MODE_RDONLY, MPI_INFO_NULL, &fh); 
+	MPI_Status status;
+	offset = modelSize * N * rank;
+
+  TRACE() << "parallel: add perturbation to initial velocity";
+  if (!fh) {
+    ERROR() << "cannot open file: " << perin;
+    exit(EXIT_FAILURE);
+  }
+
+  std::vector<float> tmp(modelSize);
+  std::vector<float> velOrig = vel.dat;
+  std::transform(velOrig.begin(), velOrig.end(), velOrig.begin(), boost::bind(velRecover<float>, _1, dx, dt));
+
+	int readSize = modelSize;
+
+  for (int iv = 0; iv < N; iv++) {
+    std::vector<float> ret(modelSize);
+		MPI_File_read_at(fh, offset, &tmp[0], readSize, MPI_FLOAT, &status);
+		offset += readSize * sizeof(&tmp[0]);
+    //ifs.read(reinterpret_cast<char *>(&tmp[0]), modelSize * sizeof(tmp[0]));
+    std::transform(tmp.begin(), tmp.end(), velOrig.begin(), ret.begin(), std::plus<float>());
+    std::transform(ret.begin(), ret.end(), ret.begin(), boost::bind(velTrans<float>, _1, dx, dt));
+    veldb[iv] = new Velocity(ret, vel.nx, vel.nz);
+  }
+
+	MPI_File_close(&fh);
+
+  return veldb;
+}
+
 std::vector<float *> generateVelSet(std::vector<Velocity *> &veldb) {
   std::vector<float *> velSet(veldb.size());
   for (size_t iv = 0; iv < veldb.size(); iv++) {
@@ -380,6 +419,15 @@ int main(int argc, char *argv[]) {
   std::vector<Velocity *> totalveldb;  /// only for rank 0
   std::vector<Velocity *> veldb(ntask); /// each process owns # of velocity
 
+
+	int size;
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	int model_size = veldb[0]->nx * veldb[0]->nz;
+  std::vector<Velocity *> veldb2(ntask); /// each process owns # of velocity
+	veldb2 = pCreateVelDB(exvel, params.perin, veldb.size(), dx, dt, rank, size);
+  EnkfAnalyze enkfAnly2(fmMethod, wlt, dobs, sigfac);
+
+	MPI_Barrier(MPI_COMM_WORLD);
   if (rank == 0) { /// sender
     totalveldb = createVelDB(exvel, params.perin, N, dx, dt);
     for (size_t i = 0; i < totalveldb.size(); i++) {
@@ -400,6 +448,12 @@ int main(int argc, char *argv[]) {
   scatterVelocity(veldb, totalveldb, params);
   DEBUG() << "dispatching velocity finished!";
 //  MPI_Barrier(MPI_COMM_WORLD);
+
+	for(int i = 0 ; i < veldb.size() ; i ++)
+		enkfAnly2.check(veldb[i]->dat, veldb2[i]->dat);
+	MPI_Barrier(MPI_COMM_WORLD);
+	exit(1);
+
 
   std::vector<Damp4t10d *> fms(ntask);
   std::vector<UpdateSteplenOp *> usl(ntask);
